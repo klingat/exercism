e4377c04aeee7bd80c9127cabff3152766b5504d
Kash Lingat <klingat@me.com>
2018-11-09 12:42:43 -0800

    Refactor leap year method

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -6,16 +6,8 @@ require 'pry'
 
 class Year
   def self.leap?(year)
-    if year % 4 == 0
-      if year % 100 == 0
-        if year % 400 == 0
-          true
-        else
-          false
-        end
-      else
-        true
-      end
+    if (year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0 ))
+      true
     else
       false
     end
